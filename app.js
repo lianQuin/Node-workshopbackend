9c9536fd553fc94c7b86def282a41f59e406e782
lilian <lian_sq@hotmail.com>
2023-12-01 21:00:55 -0300

    rutas

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const charactersRoutes = require('./src/routes/charactersRoutes');
 
 app.get('/', (req, res) => res.send ('HOLa mundo desde node'))
 
-app.use('/',charactersRoutes)
+app.use('/', charactersRoutes)
 
 //aplicacion escuchando- al puerto despues con el tema de BD hay que ocultar
 app.listen(PORT,()=> console.log(`servidor corriendo en http://localhost:${PORT}`));
--- src/routes/charactersRoutes.js
+++ src/routes/charactersRoutes.js
@@ -1,11 +1,17 @@
 //aca van a estar las rutas de las api del manejo de los datos
-const express = requiere ('express');
+const express = require ('express');
 const router = express .Router();//a este rauter despues lo vamos a leer desde app
 
 
 router.get('/characters', (req, res) => res.send('en teoria yo tendria que devolver todos los carateres'));
 //ruta para un solo caracter
-router.get('/character/:id', (req, res) => res.send('en teoria yo tendria que devolver todos los carateres con el id'));
+// lo que esta metido entre /  dentro de la ruta son los params son los pathparams - los queryparams son los que viajan van con ? clave=valor
+router.get('/character/:id', (req, res) => { 
+
+     const id = req.params.id; 
+//presta atencion a las bastick las comillas
+    res.send(`en teoria yo tendria que devolver todos los carateres con el id: ${id} `) 
+});
 
 
 router.post('/character/create',(req, res) => res.send('en teoria yo tendria que enviar los datos para crear un nuevo character'));
@@ -16,4 +22,4 @@ router.delete('/character/:id/delete',(req, res) => res.send('en teoria yo tendr
 
 //tengo que exportar y despues crear un midelwer en app que tome el router y le diga que esas rutas existen y que puede buscarlas
 
-module.export= router;
+module.exports = router;
