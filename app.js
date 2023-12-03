6051f471eb711c00aa94a288977ec008560cefb4
lilian <lian_sq@hotmail.com>
2023-12-03 20:52:42 -0300

    "haciendo challenger 4"

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require ('express');
-const app = express();
-const PORT = 4001;
+//aca toda la logica que tenga que ver con la creacion de nuestro servidor web
+
+const express = require ('express');//estamos llamando a la libreria para poder crear el servidor,
+const app = express();//usando todos los metodos de express-monta el server
+const PORT = 4001;//esto va en el archivo secreto .env
 const charactersRoutes = require('./src/routes/charactersRoutes');
 
+//mildewere a nivel aplicacion
+app.use(express.static('public_html'));
 
-
+//ruta definida, con esta informacion lo configuramos- una ruta y un puerto)
 app.get('/', (req, res) => res.send ('HOLa mundo desde node'))
 
-app.use('/', charactersRoutes)
+
 
 //aplicacion escuchando- al puerto despues con el tema de BD hay que ocultar
 app.listen(PORT,()=> console.log(`servidor corriendo en http://localhost:${PORT}`));
