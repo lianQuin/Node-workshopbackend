2849c83fe076ac0d8279bd7d1f6a6c0cec3b641d
lilian <lian_sq@hotmail.com>
2023-12-04 20:34:07 -0300

    "comence mision 5"

--- app.js
+++ app.js
@@ -3,11 +3,18 @@
 const express = require ('express');//estamos llamando a la libreria para poder crear el servidor,
 const app = express();//usando todos los metodos de express-monta el server
 const PORT = 4000;//esto va en el archivo secreto .env
+const expressLayouts= require("express-ejs-layouts");
 
 const mainRoutes = require  ('./src/routes/mainRoutes');
 const shopRoutes = require  ('./src/routes/shopRoutes');
 const adminRoutes = require ('./src/routes/adminRoutes');
 const authRoutes = require ('./src/routes/authRoutes');
+
+//esto ya es la mission 5
+app.set("view engine", "ejs");
+app.set('views', './src/views')
+app.use(expressLayouts);
+app.set("layout", "layouts/layout");
 
 //mildewere a nivel aplicacion
 app.use(express.static('public_html'));
