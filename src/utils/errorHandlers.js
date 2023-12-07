3fafde1c93a3224875fc08dd4e2edb93d55b194c
lilian <lian_sq@hotmail.com>
2023-12-07 13:07:27 -0300

    "me copie y le agregue estilo al error XD"

--- src/utils/errorHandlers.js
+++ src/utils/errorHandlers.js
@@ -1,3 +1,18 @@
 module.exports = {
-    notFoundPage: (req, res) => res.status(404).send('404: No se encuentra la página sory,este errror esta en utils'),
+    notFoundPage: (req, res) => res.status(404).send(`
+      <html>
+        <head>
+          <style>
+            body { color: red; }
+            h1 { color: blue; }
+          </style>
+        </head>
+        <body>
+          <img src="/img/branding/logo_light_horizontal.svg" alt="Logo">
+          <h1>404: No se encuentra la página</h1>
+          <p>Lo siento, este error está en utils</p>
+        </body>
+      </html>
+    `),
   }
+  
