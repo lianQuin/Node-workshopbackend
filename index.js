03f54abe0ec9989bd1636be1347c293ceade1c8b
lilian <lian_sq@hotmail.com>
2023-12-07 03:34:12 -0300

    "no esta funcionando"

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 
 
+
 /* Import de la rutas */
 
 const mainRoutes = require('./src/routes/mainRoutes');
@@ -26,12 +27,20 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, "./src/views"));
 
 /* Parsea los datos recibidos por POST */
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded());
 app.use(express.json());
 
-/* Override para habilitar métodos PUT y DELETE */
-app.use(methodOverride('_method'));
+ /*Override para habilitar métodos PUT y DELETE */
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+
+
+
+
+
 
 /* Rutas de la aplicación */
 
@@ -40,6 +49,86 @@ app.use('/shop', shopRoutes);
 app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
 
-//app.use(notFoundPage);
+app.use(notFoundPage);
 
 app.listen(PORT, () => console.log(`Servidor corriendo en: http://localhost:${PORT}`))
+
+
+/*  require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+
+const sequelize = require("./src/models/connect");
+
+// const session = require("express-session");
+const session = require("cookie-session");
+
+// app.use(
+//   session({
+//     secret: "S3cr3t01",
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
+
+app.use(
+  session({
+    keys: ["S3cr3t01", "S3cr3t02"],
+  })
+);
+
+const isLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    return res.redirect("/login");
+  }
+
+  next();
+};
+
+// const online = true;
+
+// app.use((req, res, next) => {
+//   if (online) {
+//     next();
+//   } else {
+//     res.send("En mantenimiento");
+//   }
+// });
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/src/views"));
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(require("./src/routes/authRoutes"));
+
+const mainRoutes = require("./src/routes/mainRoutes");
+app.use(mainRoutes);
+
+const adminCategoriasRoutes = require("./src/routes/admin/categoriasRoutes");
+app.use("/admin/categorias", isLogin, adminCategoriasRoutes);
+
+const adminProductosRoutes = require("./src/routes/admin/productosRoutes");
+app.use("/admin/productos", isLogin, adminProductosRoutes);
+// app.use("/admin/productos", require("./src/routes/adminProductosRoutes"));
+
+app.use((req, res, next) => {
+  res.status(404).send("La pagina no existe");
+});
+
+const PORT = 3000;
+app.listen(PORT, async () => {
+  try {
+    await sequelize.sync({ alter: true });
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(`http://localhost:${PORT}`);
+});
+*/
--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -66,3 +66,19 @@ module.exports = {
     res.redirect('/admin');
   },
 };
+
+ /* Este código es un módulo de Node.js que exporta un conjunto de controladores para manejar diferentes rutas en una aplicación web. Aquí te explico cada función:
+
+adminView: Esta función maneja la vista del administrador. Obtiene todos los elementos de ItemsService y los pasa a la vista admin.
+
+createView: Esta función maneja la vista de creación de un nuevo elemento. Obtiene todas las categorías de CategoryService y todas las licencias de LicenceService, y las pasa a la vista create.
+
+createItem: Esta función maneja la creación de un nuevo elemento. Toma los datos del cuerpo de la solicitud y los archivos adjuntos, y los pasa a ItemsService.create() para crear un nuevo elemento. Luego redirige al usuario a la vista del administrador.
+
+bulkCreate: Esta función maneja la creación masiva de elementos. Toma los datos del cuerpo de la solicitud, los convierte en un array de arrays y los pasa a ItemsService.create() para crear los elementos. Luego envía el resultado como respuesta.
+
+editView: Esta función maneja la vista de edición de un elemento. Obtiene el elemento, todas las categorías y todas las licencias, y los pasa a la vista edit.
+
+editItem: Esta función maneja la edición de un elemento. Toma los datos del cuerpo de la solicitud y el ID del elemento, y los pasa a ItemsService.edit() para editar el elemento. Luego redirige al usuario a la vista del administrador.
+
+deleteItem: Esta función maneja la eliminación de un elemento. Toma el ID del elemento y lo pasa a ItemsService.delete() para eliminar el elemento. Luego redirige al usuario a la vista del administrador. */
